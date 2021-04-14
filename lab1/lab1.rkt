1b6f5c0c68630e3715439a509e02262277c0b2d5
andrewjperry <andrewjperry@gmail.com>
2021-04-13 17:05:44 -0700

    Added some more code

--- lab1/lab1.rkt
+++ lab1/lab1.rkt
@@ -5,7 +5,7 @@
   ; A semi-colon starts a line comment.
   ; The expression below is the function body.
   (filled-rectangle n n))
-(square 5)
+(square 20)
 (define (rainbow p)
   (map (lambda (color)
          (colorize p color))
@@ -13,3 +13,25 @@
  
 
  (rainbow (square 5))
+
+(define (four p)
+  (define two-p (hc-append p p))
+  (vc-append two-p two-p))
+ 
+
+(rainbow (four (circle 10)))
+(define (checker p1 p2)
+  (let ([p12 (hc-append p1 p2)]
+        [p21 (hc-append p2 p1)])
+    (vc-append p12 p21)))
+(checker (colorize (square 10) "red")
+           (colorize (square 10) "black"))
+
+(define (checkerboard p)
+  (let* ([rp (colorize p "red")]
+         [bp (colorize p "black")]
+         [c (checker rp bp)]
+         [c4 (four c)])
+    (four c4)))
+(checkerboard (square 10))
+
