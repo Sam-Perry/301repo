534791f010e8dbae1d6a0936443d84e6c7e64390
andrewjperry <andrewjperry@gmail.com>
2021-04-14 19:37:42 -0700

    Added some more code

--- lab1/lab1.rkt
+++ lab1/lab1.rkt
@@ -1,37 +1,15 @@
 #lang slideshow
-(circle 10)
-(circle 100)
-(define (square n)
-  ; A semi-colon starts a line comment.
-  ; The expression below is the function body.
-  (filled-rectangle n n))
-(square 20)
-(define (rainbow p)
-  (map (lambda (color)
-         (colorize p color))
-       (list "red" "orange" "yellow" "green" "blue" "purple")))
- 
 
- (rainbow (square 5))
 
-(define (four p)
-  (define two-p (hc-append p p))
-  (vc-append two-p two-p))
- 
+(define (pyramid s1)
+  (define two-s (hc-append s1 s1))
+  (vc-append s1 two-s)
+  )
+(pyramid (circle 10))
 
-(rainbow (four (circle 10)))
-(define (checker p1 p2)
-  (let ([p12 (hc-append p1 p2)]
-        [p21 (hc-append p2 p1)])
-    (vc-append p12 p21)))
-(checker (colorize (square 10) "red")
-           (colorize (square 10) "black"))
+(define (nested-pyramid s1 d)
+  [ d (pyramid s1)]
+  )
 
-(define (checkerboard p)
-  (let* ([rp (colorize p "red")]
-         [bp (colorize p "black")]
-         [c (checker rp bp)]
-         [c4 (four c)])
-    (four c4)))
-(checkerboard (square 10))
+(nested-pyramid (circle 10) 2)
 
